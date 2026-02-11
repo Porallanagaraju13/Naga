4e17eca91f5e40288ed7e2348d1dbf85170f513d
Poralla Nagaraju <nagarajuporalla13@gmail.com>
2026-02-11 12:50:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Naga/",   // ⭐ VERY IMPORTANT for GitHub Pages
+  base: mode === "production" ? "/Naga/" : "/",
 
   server: {
     host: "::",
