a317d67be3553a05530d24b9917ddb09689dca9a
Poralla Nagaraju <nagarajuporalla13@gmail.com>
2026-02-11 12:33:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Chatbot from "@/components/Chatbot";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -15,14 +15,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+
+      {/* ✅ Changed BrowserRouter → HashRouter */}
+      <HashRouter>
         <Analytics />
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
+
       <Chatbot />
     </TooltipProvider>
   </QueryClientProvider>
